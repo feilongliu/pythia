73c4c531ffbf70eaef08116d83503fd0509d03be
feilongliu <liufl102@gmail.com>
2014-03-07 01:50:16 -0500

    first commit

--- unit_tests/common.h
+++ unit_tests/common.h
@@ -55,6 +55,10 @@ void assertmsg(bool cond, const char* explanation)
 void createfile(const char* filename, const unsigned int maxnum)
 {
 	std::ofstream of(filename);
+	if (!of.is_open()){
+
+               std::cout << "fail to open file " << filename << std::endl;
+        }
 	for (unsigned int i=1; i<(maxnum+1); ++i)
 	{
 		of << i << "|" << i << std::endl;
@@ -64,5 +68,7 @@ void createfile(const char* filename, const unsigned int maxnum)
 
 void deletefile(const char* filename)
 {
-	remove(filename);
+	if(remove(filename) != 0){
+		std::cout << "fail to delete file " << filename << std::endl;
+	}
 }
